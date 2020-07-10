bb22f50dca4617756dddea08d7f77ed4d4d99144
Ezekiel <iamwebwiz@gmail.com>
2020-07-10 08:36:58 +0100

    Add todos api file

---
+++ functions/APIs/todos.js
@@ -0,0 +1,16 @@
+exports.fetchAllTodos = (request, response) => {
+  todos = [
+    {
+      id: '1',
+      title: 'greeting',
+      body: 'Hello world from sharvin shah',
+    },
+    {
+      id: '2',
+      title: 'greeting2',
+      body: 'Hello2 world2 from sharvin shah',
+    },
+  ]
+
+  return response.json(todos)
+}
