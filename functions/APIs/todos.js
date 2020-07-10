734a286d68ebfcb86574d332a55ff127b00ae4c5
Ezekiel <iamwebwiz@gmail.com>
2020-07-10 10:06:56 +0100

    Update todos.js

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -1,16 +1,25 @@
-exports.fetchAllTodos = (request, response) => {
-  todos = [
-    {
-      id: '1',
-      title: 'greeting',
-      body: 'Hello world from sharvin shah',
-    },
-    {
-      id: '2',
-      title: 'greeting2',
-      body: 'Hello2 world2 from sharvin shah',
-    },
-  ]
+const { db } = require('../utilities/admin')
 
-  return response.json(todos)
+exports.fetchAllTodos = (request, response) => {
+  db.collection('todos')
+    .orderBy('createdAt', 'desc')
+    .get()
+    .then(data => {
+      let todos = []
+
+      data.forEach(document => {
+        todos.push({
+          todoId: document.id,
+          title: document.data().title,
+          body: document.data().body,
+          createdAt: document.data().createdAt,
+        })
+      })
+
+      return response.json(todos)
+    })
+    .catch(err => {
+      console.error(err)
+      return response.status(500).json({ error: err.code })
+    })
 }
