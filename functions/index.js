ef042ee3fba3c964160bd78fe174a992d74532df
Ezekiel <iamwebwiz@gmail.com>
2020-07-10 08:37:20 +0100

    Add express

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,8 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const app = require('express')()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+const { fetchAllTodos } = require('./APIs/todos')
+
+app.get('/todos', fetchAllTodos)
+
+exports.api = functions.https.onRequest(app)
