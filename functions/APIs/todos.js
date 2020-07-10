fb7c6782e92fe69180e02084d00cf64678440841
Ezekiel <iamwebwiz@gmail.com>
2020-07-10 23:10:55 +0100

    Add handler for adding new todo item

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -20,6 +20,41 @@ exports.fetchAllTodos = (request, response) => {
     })
     .catch(err => {
       console.error(err)
-      return response.status(500).json({ error: err.code })
+      response.status(500).json({ error: err.code })
+    })
+}
+
+exports.postNewTodo = (request, response) => {
+  if (request.body.body.trim() === '') {
+    return response.status(422).json({
+      body: 'Must not be empty',
+    })
+  }
+
+  if (request.body.title.trim() === '') {
+    return response.status(422).json({
+      title: 'Must not be empty',
+    })
+  }
+
+  const newTodo = {
+    title: request.body.title,
+    body: request.body.body,
+    createdAt: new Date().toISOString(),
+  }
+
+  db.collection('todos')
+    .add(newTodo)
+    .then(todo => {
+      const todoItem = newTodo
+      todoItem.id = todo.id
+
+      return response.status(201).json(todoItem)
+    })
+    .catch(err => {
+      console.error(err)
+      response
+        .status(500)
+        .json({ error: 'Something went wrong while adding todo' })
     })
 }
