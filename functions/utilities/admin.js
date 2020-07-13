edbc53fb440564bc847133d8b1a375ebe718778d
Ezekiel <iamwebwiz@gmail.com>
2020-07-13 03:27:45 +0100

    Format file

--- functions/utilities/admin.js
+++ functions/utilities/admin.js
@@ -1,12 +1,12 @@
-const admin = require('firebase-admin')
+const admin = require("firebase-admin");
 
-const serviceAccount = require(`../${process.env.FIREBASE_SERVICE_ACCOUNT}`)
+const serviceAccount = require(`../${process.env.FIREBASE_SERVICE_ACCOUNT}`);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.FIREBASE_DATABASE_URL,
-})
+});
 
-const db = admin.firestore()
+const db = admin.firestore();
 
-module.exports = { admin, db }
+module.exports = { admin, db };
