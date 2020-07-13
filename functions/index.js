0a79b5969008d645bb293236c0a208d0fb89c7af
Ezekiel <iamwebwiz@gmail.com>
2020-07-13 03:27:36 +0100

    Update index file

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
-const functions = require('firebase-functions')
-const app = require('express')()
-require('dotenv').config()
+const functions = require("firebase-functions");
+const app = require("express")();
+require("dotenv").config();
 
 const {
   fetchAllTodos,
@@ -8,19 +8,19 @@ const {
   postNewTodo,
   editTodo,
   deleteTodo,
-} = require('./APIs/todos')
+} = require("./APIs/todos");
 
-app.get('/', (request, response) =>
+app.get("/", (request, response) =>
   response.json({
-    app_name: 'Express React Firebase Todo App',
-    app_version: '1.0.0',
+    app_name: "Express React Firebase Todo App",
+    app_version: "1.0.0",
   })
-)
+);
 
-app.get('/todos', fetchAllTodos)
-app.get('/todos/:id', fetchSingleTodo)
-app.post('/todos', postNewTodo)
-app.put('/todos/:id', editTodo)
-app.delete('/todos/:id', deleteTodo)
+app.get("/todos", fetchAllTodos);
+app.get("/todos/:id", fetchSingleTodo);
+app.post("/todos", postNewTodo);
+app.put("/todos/:id", editTodo);
+app.delete("/todos/:id", deleteTodo);
 
-exports.api = functions.https.onRequest(app)
+exports.api = functions.https.onRequest(app);
