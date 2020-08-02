ecdcacdbd35ed77382946f4b6de1b09894086c79
Ezekiel <iamwebwiz@gmail.com>
2020-08-02 22:36:49 +0100

    Add implementations for signing users up and logging them into the app

--- functions/APIs/users.js
+++ functions/APIs/users.js
@@ -35,3 +35,69 @@ exports.login = (request, response) => {
         .json({ general: "Wrong credentials. Please try again" });
     });
 };
+
+exports.signup = (request, response) => {
+  const newUser = {
+    firstName: request.body.firstName,
+    lastName: request.body.lastName,
+    email: request.body.email,
+    phoneNumber: request.body.phoneNumber,
+    country: request.body.country,
+    password: request.body.password,
+    confirmPassword: request.body.confirmPassword,
+    username: request.body.username,
+  };
+
+  const { valid, errors } = validateSignupData(newUser);
+
+  if (!valid) return response.status(400).json(errors);
+
+  let token, userId;
+
+  db.doc(`/users/${newUser.username}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return response
+          .status(400)
+          .json({ username: "this username is already taken" });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idtoken) => {
+      token = idtoken;
+
+      const userCredentials = {
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        username: newUser.username,
+        phoneNumber: newUser.phoneNumber,
+        country: newUser.country,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+      };
+
+      return db.doc(`/users/${newUser.username}`).set(userCredentials);
+    })
+    .then(() => {
+      return response.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return response.status(400).json({ email: "Email already in use" });
+      } else {
+        return response
+          .status(500)
+          .json({ general: "Something went wrong, please try again" });
+      }
+    });
+};
