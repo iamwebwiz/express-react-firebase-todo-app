a3dfadcef2dfd1e738b6e8efb05b54c69bf049b0
Ezekiel <iamwebwiz@gmail.com>
2020-08-02 22:37:04 +0100

    Add data validators for registration and login

--- functions/utilities/validators.js
+++ functions/utilities/validators.js
@@ -5,8 +5,39 @@ const isEmpty = (string) => {
 exports.validateLoginData = (data) => {
   let errors = {};
 
-  if (isEmpty(data.email)) errors.email = "Email ust not be empty";
-  if (isEmpty(data.password)) errors.password = "Password must not be  empty";
+  if (isEmpty(data.email)) errors.email = "Must not be empty";
+  if (isEmpty(data.password)) errors.password = "Must not be  empty";
+
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0 ? true : false,
+  };
+};
+
+const isEmail = (email) => {
+  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return email.match(emailRegex);
+};
+
+exports.validateSignupData = (data) => {
+  let errors = {};
+
+  if (isEmpty(data.email)) {
+    errors.email = "Must not be empty";
+  } else if (!isEmail(data.email)) {
+    errors.email = "Must be valid email address";
+  }
+
+  if (isEmpty(data.firstName)) errors.firstName = "Must not be empty";
+  if (isEmpty(data.lastName)) errors.lastName = "Must not be empty";
+  if (isEmpty(data.phoneNumber)) errors.phoneNumber = "Must not be empty";
+  if (isEmpty(data.country)) errors.country = "Must not be empty";
+
+  if (isEmpty(data.password)) errors.password = "Must not be empty";
+  if (data.password !== data.confirmPassword)
+    errors.confirmPassword = "Passowrds must be the same";
+  if (isEmpty(data.username)) errors.username = "Must not be empty";
 
   return {
     errors,
